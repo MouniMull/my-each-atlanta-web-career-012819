0653649dac86e9cb59472a01da55280a5ece497c
Mouni Mullapudi <mullapudi24@gmail.com>
2019-01-28 01:50:07 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,11 @@
  collection = [1, 2, 3, 4]
 
 def my_each(array)
-
-  i = 0 
-  while i < collection.length 
-      yield [i]
+i = 0 
+  while i < array.length 
+    yield [i]
    i += 1 
+   
    
     
     
